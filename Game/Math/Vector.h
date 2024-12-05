04682e292603b32a7dc0673dd5dd39cd35196e25
Rui Jiang <anver.jiangrui@gmail.com>
2024-12-04 21:07:08 -0500

    Remove unnecessary macro checks

--- Game/Math/Vector.h
+++ Game/Math/Vector.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#if defined(__SSE4_1__) || defined(__AVX__) || defined(__AVX2__)||defined(__SSE2__) || defined(_M_IX86) || defined(_M_X64)
+#if defined(__AVX__) || defined(__AVX2__) || defined(_M_IX86) || defined(_M_X64)
 #include "Vector3p.h"
 using Vector3 = Vector3p;
 #else
